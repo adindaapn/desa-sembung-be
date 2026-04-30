de0f6fa9cd8290fc2d5771b169acd846a750386d
AdityaputraMGL <newaditya6@gmail.com>
2026-04-30 22:22:35 +0700

    fix: ganti bcrypt ke bcryptjs

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const db = require("../config/database");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const multer = require("multer");
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
